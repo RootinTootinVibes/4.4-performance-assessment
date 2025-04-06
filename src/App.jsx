463ca0616da84fb6dd38a20d1a726b0908205579
RootinTootinVibes <cullenmoore417@gmail.com>
2025-04-06 15:42:28 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Counter from './components/Counter.jsx';
+import TextField from './components/TextField.jsx';
+import TodoList from './components/TodoList.jsx';
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+      <Counter />
+      <br />
+      <TextField />
+      <br />
+      <TodoList />
+    </div>
+  )
+}
+
+export default App
